42b23b2dd063a51413bc388b681b68c9bee791f8
waitletmecook <zzula5152@gmail.com>
2025-03-13 10:36:45 +0300

    haslomaslo

--- components/shared/checkbox-filters-group.tsx
+++ components/shared/checkbox-filters-group.tsx
@@ -1,8 +1,10 @@
 "use client";
 
 import React from "react";
-import { FilterCheckbox, FilterCheckboxProps } from "./filter-checkbox";
-import { Input } from "../ui/input";
+import {  FilterCheckbox, FilterCheckboxProps } from "./filter-checkbox";
+import { Input } from "../ui";
+
+
 
 type Item = FilterCheckboxProps;
 
@@ -29,25 +31,51 @@ export const CheckboxFiltersGroup: React.FC<Props> = ({
 
 }) => {
     const [showAll, setShowAll] = React.useState(false);
+
     const [searchValue, setSearchValue] = React.useState("");
+
     const onChangeSearchInput = (e: React.ChangeEvent<HTMLInputElement>) => {
         setSearchValue(e.target.value);
-    }
+    
 }
 
 const list = showAll
-? items.filter((item) => item.text.toLowerCase().includes(searchValue.toLowerCase()))
-: defaultItems.slice(0, limit);
+    ? items.filter((item) => item.text.toLowerCase().includes(searchValue.toLowerCase()))
+    : defaultItems.slice(0, limit);
 
 return (
     <div className={className}>
         <p className="font-bold mb-3">{title}</p>
+
         {showAll && (
-        <div className="mb-5">
-            <Input
-            onChange={onChangeSearchInput}
-            className="bg-gray-50 border-none"
-            />
-        )} 
+            <div className="mb-5">
+                <Input
+                    onChange={onChangeSearchInput}
+                    placeholder={searchInputPlaceholder}
+                    className="bg-gray-50 border-none"
+                />
+            </div>
+        )}
+
+        <div className="flex flex-col gap-4 max-h-96 pr-2 overflow-auto scrollbar">
+            {list.map((item, index) => (
+                <FilterCheckbox
+                    onCheckedChange={(ids) => console.log(ids)}
+                    checked={false}
+                    key = { index }
+                    value = { item.value }
+                    text = { item.text }
+                    endAbornment = { item.endAbornment }
+                />
+        ))}
+        </div>
+
+        {items.length > limit && (
+            <div className={showAll ? 'border-t border-t-neutral-100 mt-4' : ""}>
+                <button onClick={() => setShowAll(!showAll)} className="text-primary mt-3">
+                    {showAll ? 'Скрыть' : '+ Показать все'}
+                </button>
+            </div>
+        )}
     </div>
-)
+)}
--- components/shared/filters.tsx
+++ components/shared/filters.tsx
@@ -1,3 +1,4 @@
+import { CheckboxFiltersGroup } from "./checkbox-filters-group"
 import { FilterCheckbox } from "./filter-checkbox";
 import { Title } from "./title";
 
@@ -6,22 +7,54 @@ interface Props {
 }
 
 export const Filtres: React.FC<Props> = ({ className }) => {
-    return(
+    return (
         <div>
-            <Title text="Фильтрация" size="lg" className="font-bold"/>  
-        <div>
-      <FilterCheckbox value="1" text="Можно собирать"/>
+            <Title text="Фильтрация" size="lg" className="font-bold" />
+            <div>
+                <FilterCheckbox value="1" text="Можно собирать" />
 
-        <FilterCheckbox value="2" text="Новинки"/>
-        <div className="mt-5 border-y border-y-neutral-100 py-6 pb-7">
-            <p className="font-bold mb-3">Цена от и до:</p>
-            <div className="flex gap-3 mb-5">
-                <input type="number" placeholder="0" min={0} max={5000}  />
-                <input type="number" placeholder="5000" min={100} max={5000}  />
+                <FilterCheckbox value="2" text="Новинки" />
             </div>
+            <div className="mt-5 border-y border-y-neutral-100 py-6 pb-7">
+                <p className="font-bold mb-3">Цена от и до:</p>
+                <div className="flex gap-3 mb-5">
+                    <input type="number" placeholder="0" min={0} max={5000} />
+                    <input type="number" placeholder="5000" min={100} max={5000} />
+                </div>
+            </div>
+            
+            <CheckboxFiltersGroup
+                className="mt-5"
+                title="Ингредиенты"
+                limit={6}
+                defaultItems={[
+                    {
+                        text: "Сырный соус",
+                        value: "1",
+                    },
+                    {
+                        text: "Моццарелла",
+                        value: '2',
+                    },
+                    {
+                        text: 'Чеснок',
+                        value: "3",
+                    },
+                ]}
+                items={[
+                    {
+                        text: "Сырный соус",
+                        value: '1',
+                    },
+                    {
+                        text: 'Моццарелла',
+                        value: "2",
+                    },
+
+                ]}
+            />
         </div>
-      
-        </div>
-        </div>
+
+
     );
 }
